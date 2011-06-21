5a015c4ed4ff167223f0ba7a0fc3aec465d69739
Jason Polites <jason.polites@appmakr.com>
2011-06-21 14:31:35 -0700

    Added integration test project

---
+++ integration-test/src/com/socialize/sample/integrationtest/SocializeRobotiumTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2011 Socialize Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.socialize.sample.integrationtest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.jayway.android.robotium.solo.Solo;
+import com.socialize.sample.Main;
+
+/**
+ * @author Jason Polites
+ */
+public class SocializeRobotiumTest extends ActivityInstrumentationTestCase2<Main> {
+
+	protected Solo solo;
+	
+	protected String consumerKey;
+	protected String consumerSecret;
+	protected String url;
+	
+	protected Properties properties;
+	
+	public SocializeRobotiumTest() {
+		super("com.socialize.sample", Main.class);
+	}
+	
+	public void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+		loadConfig();
+		solo.clickOnButton(0);
+		
+		assertNotNull(consumerKey);
+		assertNotNull(consumerSecret);
+		assertNotNull(url);
+	}
+	
+	public void testAuthenticate() {
+		authenticate();
+		solo.waitForText("SUCCESS", 1, 5);
+		assertTrue(solo.searchText("SUCCESS"));
+	}
+	
+	public void testAddComment() {
+		authenticate();
+		solo.clickOnButton("Comment");
+		solo.waitForActivity("CommentActivity", 5);
+	}
+	
+	protected void authenticate() {
+		solo.clearEditText(0);
+		solo.enterText(0, url);
+		
+		solo.clearEditText(1);
+		solo.enterText(1, consumerKey);
+		
+		solo.clearEditText(2);
+		solo.enterText(2, consumerSecret);
+		
+		solo.clickOnButton(0);
+	}
+	
+	private void loadConfig() throws IOException {
+		InputStream in = null;
+		try {
+			in = getActivity().getAssets().open("integration-test.conf"); // located in sample app
+			
+			properties = new Properties();
+			properties.load(in);
+			
+			consumerKey = properties.getProperty("socialize.consumer.key");
+			consumerSecret = properties.getProperty("socialize.consumer.secret");
+			url = properties.getProperty("socialize.api.url");
+		}
+		finally {
+			if (in != null) {
+				in.close();
+			}
+		}
+	}
+}
