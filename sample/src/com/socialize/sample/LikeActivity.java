1fbf628591239608d67132b34abd98f78dc97ddd
Jason Polites <jason.polites@getsocialize.com>
2011-06-23 16:47:42 -0700

    Added Delete Like to sample app

--- sample/src/com/socialize/sample/LikeActivity.java
+++ sample/src/com/socialize/sample/LikeActivity.java
@@ -12,6 +12,7 @@ import com.socialize.Socialize;
 import com.socialize.entity.Like;
 import com.socialize.error.SocializeException;
 import com.socialize.listener.like.LikeAddListener;
+import com.socialize.listener.like.LikeDeleteListener;
 import com.socialize.sample.util.ErrorHandler;
 
 public class LikeActivity extends Activity {
@@ -26,6 +27,9 @@ public class LikeActivity extends Activity {
 		final TextView txtLikeCreateResult = (TextView) findViewById(R.id.txtLikeCreateResult);
 		
 		final Button btnLikeCreate = (Button) findViewById(R.id.btnLikeCreate);
+		final Button btnLikeDelete = (Button) findViewById(R.id.btnLikeDelete);
+		
+		btnLikeDelete.setEnabled(false);
 		
 		final TextView txtLikeIdCreated = (TextView) findViewById(R.id.txtLikeIdCreated);
 		final TextView txtLikeDateCreated = (TextView) findViewById(R.id.txtLikeDateCreated);
@@ -53,6 +57,7 @@ public class LikeActivity extends Activity {
 						@Override
 						public void onCreate(Like entity) {
 							btnLikeCreate.setEnabled(true);
+							btnLikeDelete.setEnabled(true);
 							txtLikeCreateResult.setText("SUCCESS");
 							
 							if(entity.getId() != null) {
@@ -64,6 +69,40 @@ public class LikeActivity extends Activity {
 							}
 						}
 					});
+				}
+			});
+			
+			btnLikeDelete.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					txtLikeCreateResult.setText("");
+					
+					String id =  txtLikeIdCreated.getText().toString();
+					
+					if(id != null) {
+						btnLikeDelete.setEnabled(false);
+						
+						Socialize.getSocialize().deleteLike(Integer.parseInt(id), new LikeDeleteListener() {
+							
+							@Override
+							public void onError(SocializeException error) {
+								txtLikeCreateResult.setText("FAIL: " + ErrorHandler.handleApiError(LikeActivity.this, error));
+								btnLikeDelete.setEnabled(true);
+							}
+							
+							@Override
+							public void onDelete() {
+								txtLikeCreateResult.setText("SUCCESS");
+								txtLikeIdCreated.setText("");
+								txtLikeDateCreated.setText("");
+							}
+						});
+					}
+					else {
+						txtLikeCreateResult.setText("FAIL: No ID");
+					}
 				}
 			});
 		}
