ad331a8f7e4f934c33599f38fbaa68be87bd073c
Jason Polites <jason.polites@getsocialize.com>
2011-07-21 15:19:26 -0700

    Added finish() call in tearDown of i-tests

--- integration-test/src/com/socialize/sample/integrationtest/SocializeRobotiumTest.java
+++ integration-test/src/com/socialize/sample/integrationtest/SocializeRobotiumTest.java
@@ -56,6 +56,9 @@ public abstract class SocializeRobotiumTest extends ActivityInstrumentationTestC
 		catch (Throwable e) {
 			e.printStackTrace();
 		}
+		
+		getActivity().finish();
+		
 		super.tearDown();
 	}
 
