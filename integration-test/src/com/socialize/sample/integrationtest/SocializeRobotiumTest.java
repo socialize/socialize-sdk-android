f3e93747adb975066d8ff65ede2a81f0372b1ae0
Peerakit Champ Somsuk <beyourwish@gmail.com>
2011-08-23 17:16:57 -0700

    add default URL to be more complecated

--- integration-test/src/com/socialize/sample/integrationtest/SocializeRobotiumTest.java
+++ integration-test/src/com/socialize/sample/integrationtest/SocializeRobotiumTest.java
@@ -48,10 +48,10 @@ import com.socialize.sample.Main;
 public abstract class SocializeRobotiumTest extends ActivityInstrumentationTestCase2<Main> {
 
 	public static final int DEFAULT_TIMEOUT_SECONDS = 50;
-	public static final String DEFAULT_ENTITY_URL = "http://socialize.integration.tests.com";
+	public static final String DEFAULT_ENTITY_URL = "http://socialize.integration.tests.com?somekey=somevalue&anotherkey=anothervalue";
 	public static final String DEFAULT_APPLICATION_NAME = "Socialize Android Sample App DO NOT DELETE";
 	public static final String DEFAULT_GET_ENTITY = "http://entity1.com";
-
+	
 	protected Solo robotium;
 	protected InputMethodManager imm = null;
 
