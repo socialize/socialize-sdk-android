c494891724768e2ced99bbab9a329b7c612cc54c
Jason Polites <jason.polites@getsocialize.com>
2011-08-26 15:43:00 -0700

    Updated getting started guide

--- ui-sample/src/com/socialize/ui/sample/SampleActivity.java
+++ ui-sample/src/com/socialize/ui/sample/SampleActivity.java
@@ -5,27 +5,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.socialize.ui.SocializeUI;
 
 public class SampleActivity extends Activity {
-	@Override
+
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.main);
 		
-		final EditText txtEntity = (EditText) findViewById(R.id.txtEntity);
-		Button btn = (Button) findViewById(R.id.btnCommentView);
+		// These can be specified in a config file 
+		// called socialize.properties and places in your assets path.
+		final String consumerKey = "807de77a-9981-450a-b0fc-c239c9c3f9b0";
+		final String consumerSecret = "0ccc078d-a628-49d0-97ac-bcc892d9666f";
+		
+		// Your entity key.  May be passed as a Bundle parameter to your activity
+		final String entityKey = "http://someurl.com";
+		
+		final Button btn = (Button) findViewById(R.id.btnCommentView);
 		
 		btn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String entityKey = txtEntity.getText().toString();
-				
-//				SocializeUI.getInstance().setFacebookUserCredentials(this, "blah", "blah");
-//				SocializeUI.getInstance().setSocializeCredentials("807de77a-9981-450a-b0fc-c239c9c3f9b0", "0ccc078d-a628-49d0-97ac-bcc892d9666f");
+				SocializeUI.getInstance().setSocializeCredentials(consumerKey, consumerSecret);
 				SocializeUI.getInstance().showCommentView(SampleActivity.this, entityKey);
 			}
 		});
