17df31c2b0e96516d91a8596743fcab80ee176ac
Jason Polites <jason.polites@getsocialize.com>
2011-08-28 14:45:00 -0700

    Added ANT build for UI tests

--- ui-test/src/com/socialize/ui/test/CommentAdapterTest.java
+++ ui-test/src/com/socialize/ui/test/CommentAdapterTest.java
@@ -11,7 +11,7 @@ public class CommentAdapterTest extends SocializeUITest {
 
 	@SuppressWarnings("unchecked")
 	@UsesMocks ({List.class})
-	public void testIsDisplayLoading() {
+	public void testIsDisplayLoadingFalse() {
 		
 		List<Comment> comments = AndroidMock.createMock(List.class);
 		
@@ -29,4 +29,119 @@ public class CommentAdapterTest extends SocializeUITest {
 		AndroidMock.verify(comments);
 	}
 	
+	@SuppressWarnings("unchecked")
+	@UsesMocks ({List.class})
+	public void testIsDisplayLoadingTrue() {
+		
+		List<Comment> comments = AndroidMock.createMock(List.class);
+		
+		AndroidMock.expect(comments.size()).andReturn(1);
+		
+		CommentAdapter adapter = new CommentAdapter(getContext());
+		
+		adapter.setLast(false);
+		adapter.setComments(comments);
+		
+		AndroidMock.replay(comments);
+		
+		assertTrue(adapter.isDisplayLoading());
+		
+		AndroidMock.verify(comments);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@UsesMocks ({List.class})
+	public void testGetCountWithLoading() {
+		int count = 10;
+		List<Comment> comments = AndroidMock.createMock(List.class);
+		
+		AndroidMock.expect(comments.size()).andReturn(count);
+		
+		CommentAdapter adapter = new CommentAdapter(getContext()) {
+			@Override
+			public boolean isDisplayLoading() {
+				return true;
+			}
+		};
+		
+		adapter.setLast(false);
+		adapter.setComments(comments);
+		
+		AndroidMock.replay(comments);
+		
+		assertEquals(count+1, adapter.getCount());
+		
+		AndroidMock.verify(comments);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@UsesMocks ({List.class})
+	public void testGetCountWithoutLoading() {
+		int count = 10;
+		List<Comment> comments = AndroidMock.createMock(List.class);
+		
+		AndroidMock.expect(comments.size()).andReturn(count);
+		
+		CommentAdapter adapter = new CommentAdapter(getContext()) {
+			@Override
+			public boolean isDisplayLoading() {
+				return false;
+			}
+		};
+		
+		adapter.setLast(false);
+		adapter.setComments(comments);
+		
+		AndroidMock.replay(comments);
+		
+		assertEquals(count, adapter.getCount());
+		
+		AndroidMock.verify(comments);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@UsesMocks ({List.class})
+	public void testGetItemWithinSize() {
+		Comment item = new Comment();
+		int position = 69;
+		List<Comment> comments = AndroidMock.createMock(List.class);
+		
+		AndroidMock.expect(comments.get(position)).andReturn(item);
+		AndroidMock.expect(comments.size()).andReturn(position+1);
+		
+		CommentAdapter adapter = new CommentAdapter(getContext());
+		
+		adapter.setLast(false);
+		adapter.setComments(comments);
+		
+		AndroidMock.replay(comments);
+		
+		assertSame(item, adapter.getItem(position));
+		
+		AndroidMock.verify(comments);
+		
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	@UsesMocks ({List.class})
+	public void testGetItemOutsideSize() {
+		int position = 69;
+		List<Comment> comments = AndroidMock.createMock(List.class);
+		
+		AndroidMock.expect(comments.size()).andReturn(position);
+		
+		CommentAdapter adapter = new CommentAdapter(getContext());
+		
+		adapter.setLast(false);
+		adapter.setComments(comments);
+		
+		AndroidMock.replay(comments);
+		
+		assertNull(adapter.getItem(position));
+		
+		AndroidMock.verify(comments);
+		
+		
+	}
 }
