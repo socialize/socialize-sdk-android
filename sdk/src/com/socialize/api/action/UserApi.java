6607a67854f98e9f49994db3b46f91c6222e5db2
Jason Polites <jason.polites@getsocialize.com>
2011-09-19 10:59:58 -0700

    Added UserApi

---
+++ sdk/src/com/socialize/api/action/UserApi.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2011 Socialize Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.socialize.api.action;
+
+import com.socialize.api.SocializeApi;
+import com.socialize.api.SocializeSession;
+import com.socialize.entity.User;
+import com.socialize.listener.user.UserListener;
+import com.socialize.provider.SocializeProvider;
+
+/**
+ * @author Jason Polites
+ */
+public class UserApi extends SocializeApi<User, SocializeProvider<User>> {
+
+	public static final String ENDPOINT = "/user/";
+	
+	public UserApi(SocializeProvider<User> provider) {
+		super(provider);
+	}
+	
+	public void getUser(SocializeSession session, int id, UserListener listener) {
+		getAsync(session, ENDPOINT, String.valueOf(id), listener);
+	}
+}
