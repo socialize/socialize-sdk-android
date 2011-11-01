c44bac87fa8597b74b8386be3e229d1ee620ee08
Jason Polites <jason.polites@gmail.com>
2011-11-01 14:11:34 -0700

    Fixed build error

--- ui-test/src/com/socialize/ui/test/EntityViewTest.java
+++ ui-test/src/com/socialize/ui/test/EntityViewTest.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import com.google.android.testing.mocking.AndroidMock;
 import com.google.android.testing.mocking.UsesMocks;
