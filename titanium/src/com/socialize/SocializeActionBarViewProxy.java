f406262299e518c6d096c80281cc206024670530
Jason Polites <jason.polites@gmail.com>
2011-12-16 16:57:51 -0800

    Added titanium module

---
+++ titanium/src/com/socialize/SocializeActionBarViewProxy.java
@@ -0,0 +1,35 @@
+package com.socialize;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.proxy.TiViewProxy;
+import org.appcelerator.titanium.view.TiUIView;
+
+import android.app.Activity;
+
+public class SocializeActionBarViewProxy extends TiViewProxy {
+	
+	private String entityKey;
+	
+	private SocializeActionBarView view;
+	private TiViewProxy parent;
+
+	public SocializeActionBarViewProxy(TiContext tiContext, TiViewProxy parent, String entityKey) {
+		super(tiContext);
+		this.entityKey = entityKey;
+		this.parent = parent;
+	}
+
+	@Override
+	public TiUIView createView(Activity activity) {
+		view = new SocializeActionBarView(this, parent, activity, entityKey);
+		return view;
+	}
+
+	public String getEntityKey() {
+		return entityKey;
+	}
+
+	public void setEntityKey(String entityKey) {
+		this.entityKey = entityKey;
+	}
+}
