7354d2565b08aba6096911f2f385e5615f55b33d
Jason Polites <jason.polites@gmail.com>
2012-02-09 11:10:20 -0800

    Fixed notification failure

---
+++ sdk/src/com/socialize/auth/twitter/TwitterOAuthProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011 Socialize Inc.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.socialize.auth.twitter;
+
+import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
+
+import org.apache.http.client.HttpClient;
+
+/**
+ * @author Jason Polites
+ *
+ */
+public class TwitterOAuthProvider extends CommonsHttpOAuthProvider {
+	
+	private static final long serialVersionUID = 3804709921717607933L;
+	
+	public static final String REQUEST_TOKEN_ENDPOINT = "https://api.twitter.com/oauth/request_token";
+	public static final String ACCESS_TOKEN_ENDPOINT = "https://api.twitter.com/oauth/access_token";
+	public static final String AUTHORIZE_ENDPOINT = "https://api.twitter.com/oauth/authorize";
+	
+	public TwitterOAuthProvider(HttpClient httpClient) {
+		super(REQUEST_TOKEN_ENDPOINT, ACCESS_TOKEN_ENDPOINT, AUTHORIZE_ENDPOINT, httpClient);
+	}
+
+	public TwitterOAuthProvider() {
+		super(REQUEST_TOKEN_ENDPOINT, ACCESS_TOKEN_ENDPOINT, AUTHORIZE_ENDPOINT);
+	}
+}
