6d1322f7993be3f2e21f5d2f206b1f685d140d8d
Jason Polites <jason.polites@gmail.com>
2012-05-03 14:19:28 -0700

    Updated version

--- sdk/src/com/socialize/Socialize.java
+++ sdk/src/com/socialize/Socialize.java
@@ -36,7 +36,7 @@ import com.socialize.log.SocializeLogger.LogLevel;
 public class Socialize {
 	
 	// This will be set during the build process
-	public static final String VERSION = "1.6.5";
+	public static final String VERSION = "1.6.6";
 	
 	public static final String ENTITY_OBJECT = "socialize.entity";
 	public static final String ENTITY_ID = "socialize.entity.id";
