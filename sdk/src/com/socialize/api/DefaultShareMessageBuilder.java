054bfea35bbc3794c095b31512e33ed2c64f40b4
Jason Polites <jason.polites@gmail.com>
2012-06-04 13:30:15 -0700

    Removed parenthesis from share text

--- sdk/src/com/socialize/api/DefaultShareMessageBuilder.java
+++ sdk/src/com/socialize/api/DefaultShareMessageBuilder.java
@@ -142,9 +142,8 @@ public class DefaultShareMessageBuilder implements ShareMessageBuilder {
 			}
 			else {
 				builder.append(appUtils.getAppName());
-				builder.append(" (");
+				builder.append(" ");
 				builder.append(urlSet.getAppUrl());
-				builder.append(")");
 			}
 			
 //			if(config.isBrandingEnabled()) {
