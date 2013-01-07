5b09c8835756d578e1d8df93e4f6793b0db26874
Jason Polites <jason.polites@gmail.com>
2013-01-07 15:25:09 -0800

    Updates to notification logic changes

--- test/src/com/socialize/test/unit/notifications/NotificationCheckerTest.java
+++ test/src/com/socialize/test/unit/notifications/NotificationCheckerTest.java
@@ -61,6 +61,7 @@ public class NotificationCheckerTest extends SocializeActivityTest {
 		
 		// Base mocks
 		AndroidMock.expect(config.getBooleanProperty(SocializeConfig.SOCIALIZE_CHECK_NOTIFICATIONS, true)).andReturn(true).anyTimes();
+		AndroidMock.expect(config.getBooleanProperty(SocializeConfig.SOCIALIZE_NOTIFICATIONS_REQUEST_ENABLED, true)).andReturn(true).anyTimes();
 		AndroidMock.expect(session.getUser()).andReturn(user).anyTimes();
 		AndroidMock.expect(appUtils.isNotificationsAvailable(context)).andReturn(true).anyTimes();
 		
@@ -87,7 +88,6 @@ public class NotificationCheckerTest extends SocializeActivityTest {
 		assertTrue(checker.checkRegistrations(context, session));
 		
 		AndroidMock.verify(appUtils, notificationRegistrationSystem, notificationRegistrationState, config, session);
-		
 	}
 	
 	@UsesMocks ({
