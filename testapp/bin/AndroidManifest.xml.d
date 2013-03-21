a8bd34c2de787b61eb709bf016e7fbf2130e60a6
Jason Polites <jason.polites@gmail.com>
2013-03-21 16:59:49 -0700

    Removed bin/gen from test app

--- testapp/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/testapp/bin/AndroidManifest.xml : \
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/testapp/AndroidManifest.xml \
--- testapp/bin/testapp-instrumented-unaligned.apk.d
+++
@@ -1,9 +0,0 @@
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/testapp/bin/testapp-instrumented-unaligned.apk : \
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/testapp/bin/testapp.ap_ \
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/testapp/bin/classes.dex \
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/sdk/bin/classes.jar \
-/Users/jasonpolites/dev/workspace/android-ioc/core/bin/classes.jar \
-/Users/jasonpolites/dev/workspace/facebook-android-sdk/facebook/bin/classes.jar \
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/sdk/libs/commons-codec-1.4.jar \
-/Users/jasonpolites/dev/workspace/socialize-sdk-android/sdk/libs/android-support-v4.jar \
-/android/tools/lib/emma_device.jar \
