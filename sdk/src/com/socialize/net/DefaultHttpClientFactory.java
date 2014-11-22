86abfac602f36b70cba9cc9b67d37447b6637e04
lynksant <lynksant@gmail.com>
2014-11-22 09:53:41 +0200

    Fix unsecure SSL connection

--- sdk/src/com/socialize/net/DefaultHttpClientFactory.java
+++ sdk/src/com/socialize/net/DefaultHttpClientFactory.java
@@ -74,12 +74,6 @@ public class DefaultHttpClientFactory implements HttpClientFactory {
 				logger.debug("Initializing " + getClass().getSimpleName());
 			}
 			
-	        KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-	        trustStore.load(null, null);
-
-	        SSLSocketFactory sf = new NaiveSSLSocketFactory(trustStore);
-	        sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-
 	        params = new BasicHttpParams();
 	        HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
 	        HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
@@ -89,7 +83,7 @@ public class DefaultHttpClientFactory implements HttpClientFactory {
 	        
 	        SchemeRegistry registry = new SchemeRegistry();
 	        registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-	        registry.register(new Scheme("https", sf, 443));
+	        registry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
 
 	        connectionManager = new ThreadSafeClientConnManager(params, registry);
 	        
